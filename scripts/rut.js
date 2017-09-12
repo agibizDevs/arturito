4f1b87e226d96500e2b9f645fbc4676df6b009a7
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-09-12 18:57:27 -0300

    adding script that send a ramdom chilean rut

---
+++ scripts/rut.js
@@ -0,0 +1,22 @@
+// Description:
+//  entrega un número de RUT válido aleatorio
+
+// Commands:
+//   hubot dame un rut
+
+// Author:
+//   @cvicuna
+
+var Rut     = require("rutjs");
+
+var generar = function(){
+  var num_aleatorio = Math.round(Math.random()*(25000000-5000000))+5000000;
+  var rut = new Rut(num_aleatorio.toString(), true);
+  return rut.getNiceRut()
+}
+
+module.exports = function(robot) {
+  robot.respond(/dame un rut/i, function(res) {
+    res.send( "Un RUT: " + generar() );
+  });
+};
