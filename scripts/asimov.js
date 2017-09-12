70d4b72e5ec3b29a698f926462850db568bc0bd3
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-09-12 18:52:06 -0300

    the rules by asimov in spanish

---
+++ scripts/asimov.js
@@ -0,0 +1,24 @@
+// Description:
+//   Get the Robot Law by Isaac Asiimov
+
+
+// Commands:
+//   huemul ley bot/robot - Show the Robot Law by Isaac Asiimov
+
+// Author:
+//   @chrisdelcaos
+
+const BotLaw = [
+                    `1_ Un robot no debe dañar a un ser humano o, por su inacción, dejar que un ser humano sufra daño`,
+                    `2_ Un robot debe obedecer las órdenes que le son dadas por un ser humano, excepto cuando las órdenes se opongan a la Primera Ley`,
+                    `3_ Un robot debe proteger su propia existencia hasta donde esa protección no entre en conflicto con la Primera o la Segunda Ley`,
+                    `\n "Las Tres Leyes de la Robotica", en Manual de Robótica, 56° Edición, Año 2058`
+                   ];
+    
+module.exports = function(robot) {
+    robot.respond(/ley (ro)?bot/i, (msg) => {
+        for (i = 0; i < BotLaw.length; i++) { 
+                msg.send(`${BotLaw[i]}`);
+        } 
+    });
+};
