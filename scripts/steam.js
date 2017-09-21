d90312fc79ab2ddea9725d534c652d41ca87803d
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-09-21 14:49:09 -0300

    adding spacials

--- scripts/steam.js
+++ scripts/steam.js
@@ -19,7 +19,6 @@ module.exports = (robot) => {
     return new Promise((resolve, reject) => {
 
     let request
-
         if (header) {
             request = robot.http(uri).header(header.key, header.value)
         } else {
@@ -32,28 +31,38 @@ module.exports = (robot) => {
         }
         resolve(body);
         });
-
     });
   }
 
   const getDailyId = () => {
-
     return getBody('http://store.steampowered.com').then(body => {
-
       const $ = cheerio.load(body);
       const idAttr = $('.dailydeal_desc .dailydeal_countdown').attr('id');
       return idAttr.substr(idAttr.length - 6);
+    });
+  }
 
+  const getSpecials = () => {
+    return getBody('http://store.steampowered.com/search/?specials=1').then(body => {
+      const $ = cheerio.load(body);
+      //const idAttr = $('.search_result_row').attr('data-ds-appid');
+      var games = [];
+      $('.search_result_row').each(function(i, elem) {
+        games[i] = $(this).attr('data-ds-appid');
+      });
+      return games;
+      /* for (var i = 0; i < 10; i++) { 
+            console.log(`https://store.steampowered.com/app/${games[i]}`);
+      }  */ 
+      //console.log(games);
+      //<http://www.foo.com|www.foo.com>
     });
   }
 
   const getPrice = id => {
-
     const cookie = 'steamCountry=CL%7Cb8a8a3da46a6c324d177af2855ca3d9b;timezoneOffset=-10800,0;';
     const uri = `http://store.steampowered.com/api/appdetails/?appids=${id}&cc=CL`;
-
     return getBody(uri, {key: 'cookie', value: cookie}).then(body => {
-
       const game = JSON.parse(body)[id].data;
       const name = game.name;
       const price = game.price_overview;
@@ -61,28 +70,39 @@ module.exports = (robot) => {
       const initial = price.initial / 100;
       const discount = price.discount_percent;
       return {name: name, final: final, initial: initial, discount: discount, uri: `https://store.steampowered.com/app/${id}`};
-
     });
-
   }
 
   robot.respond(/steam(.*)/i, (msg) => {
 
     const args = msg.match[1].split(' ')[1];
 
-    if (args !== 'daily') {
-      return msg.send(`Para obtener la oferta del día en _Steam_ debes usar el comando: *huemul steam daily*`)
+    if (args == 'specials') {
+      getSpecials().then(data => {
+        for (var i = 0; i < 10; i++) { 
+          getPrice(data[i]).then(data => {
+            msg.send(`Cacha hermano!: ${data.name}, a sólo $CLP ${data.final}. Valor original $CLP ${data.initial}, eso es un -${data.discount}%! <${data.uri}|link>`);
+          })
+            //console.log(`https://store.steampowered.com/app/${data[i]}`);
+        }
+        //msg.send(`¡Lorea la oferta del día!: ${data.name}, a sólo $CLP ${data.final}. Valor original $CLP ${data.initial}, eso es un -${data.discount}%! ${data.uri}`);
+      }).catch(err => {
+        msg.send('Actualmente _Steam_ no responde.');
+        robot.emit('error', err || new Error(`Status code ${res.statusCode}`), msg)
+      });
     }
 
-    getDailyId().then(getPrice).then(data => {
+    if (args == 'daily') {
 
-      msg.send(`¡Lorea la oferta del día!: ${data.name}, a sólo $CLP ${data.final}. Valor original $CLP ${data.initial}, eso es un -${data.discount}%! ${data.uri}`);
-    
-    }).catch(err => {
+      getDailyId().then(getPrice).then(data => {
+        msg.send(`¡Lorea la oferta del día!: ${data.name}, a sólo $CLP ${data.final}. Valor original $CLP ${data.initial}, eso es un -${data.discount}%! ${data.uri}`);
+      }).catch(err => {
+        msg.send('Actualmente _Steam_ no responde.');
+        robot.emit('error', err || new Error(`Status code ${res.statusCode}`), msg)
+      });
+      //return msg.send(`Para obtener la oferta del día en _Steam_ debes usar el comando: *huemul steam daily*`)
+    }
 
-      msg.send('Actualmente _Steam_ no responde.');
-      robot.emit('error', err || new Error(`Status code ${res.statusCode}`), msg)
-    
-    });
+
   });
 };
