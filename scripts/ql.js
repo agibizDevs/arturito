2899e19417e322a02202d94742049e21460e2029
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-09-25 11:31:34 -0300

    adding ql.js

---
+++ scripts/ql.js
@@ -0,0 +1,34 @@
+
+// Description:
+//   arturito chorizo
+
+// Dependencies:
+//   None
+
+// Configuration:
+//   None
+
+// Commands:
+//   arturito ql|rql|ctm|m[áa]tate|culiao|reculiao|remilculiao
+
+// Author:
+//   @cvicuna
+
+const wea = [
+    `Cha, pero pa que po!`,
+    `Weno y que pasa perkin ql!`,
+    `Te tiraste a la salida cochino ql!`,
+    `Pa' que tan zarpao peaso e' logi!`,
+    `Seguro vo' soy muy vio zhii!`,
+    `No pesco a wnes!`,
+    `Y tu hermana?`,
+    `Seguro vo soy vioh!`,
+    `Puros giles!`,
+    `Ando Tapizao!`,
+];
+
+module.exports = function(robot) {
+
+  return robot.respond(/ql|rql|ctm|m[áa]tate|culiao|reculiao|remilculiao/gi, msg => msg.send( msg.random(wea)));
+
+};
