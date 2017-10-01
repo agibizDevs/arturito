4f20b1cf180adf49b94068ac7c78f4bece8e6512
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-09-30 21:55:46 -0300

    test24

--- scripts/users.js
+++ scripts/users.js
@@ -34,7 +34,6 @@ module.exports = (robot) => {
     const name = msg.match[1].split(' ')[1];
     var users = robot.brain.usersForFuzzyName(name);
     var users2 = robot.brain.userForName(name);
-    var users3 = robot.brain.userForFuzzyName(name);
     //var users = getUserName(name);
     if(users != null && users.length > 0){
         let user = users[0];
@@ -52,16 +51,7 @@ module.exports = (robot) => {
         console.log(user);
     }     
     else{
-        msg.send(`no se encontraron datos para ${name}`);
-    }
-    if(users3 != null && users3.length > 0){
-        let user = users3[0];
-        //# Do something interesting here..
-        msg.send(`${name} is user2 - ${JSON.stringify(user)}`);
-        console.log(user);
-    }     
-    else{
-        msg.send(`no se encontraron datos para ${name}`);
+        msg.send(`no se encontraron datos2 para ${name}`);
     }
   });
 };
