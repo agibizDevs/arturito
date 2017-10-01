78fe80b60ab41a94517358c3ac1c3d546011e875
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-09-30 22:43:25 -0300

    test29

--- scripts/users.js
+++ scripts/users.js
@@ -15,22 +15,7 @@
 
 module.exports = (robot) => {
 
-  const getUserName = function(token){
-    return new Promise((resolve, reject) => {
-        let user
-        if (user = robot.brain.userForName(token)) {
-            console.log('userForNAME');
-          return resolve(user);
-        }
-
-        if (user = robot.brain.usersForFuzzyName(token)) {
-            console.log('usersForFuzzyName');
-            return resolve(user)
-        }
-      })
-  }
-
-  const getUsernameById = function(id){
+/*   const getUsernameById = function(id){
     const name;
     if(id != null){
         const users = robot.brain.users();
@@ -40,11 +25,11 @@ module.exports = (robot) => {
             }
         });
     }
-  }
+  } */
 
   robot.respond(/user(.*)/i, (msg) => {
     const name = msg.match[1].split(' ')[1];
-    const users = robot.brain.users()
+    const users = robot.brain.users();
     const nameId;
     if(name != null){
         Object.keys(users).forEach(k => {
@@ -54,27 +39,5 @@ module.exports = (robot) => {
             }
         });
     }
-
-/*     var users = robot.brain.usersForFuzzyName(name);
-    var users2 = robot.brain.userForName(name);
-    //var users = getUserName(name);
-    if(users != null && users.length > 0){
-        let user = users[0];
-        //# Do something interesting here..
-        msg.send(`${name} is user - ${JSON.stringify(users)}`);
-        console.log(user);
-    }   
-    else{
-        msg.send(`no se encontraron datos para ${name}`);
-    }
-    if(users2 != null && users2.length > 0){
-        let user = users2[0];
-        //# Do something interesting here..
-        msg.send(`${name} is user2 - ${JSON.stringify(users)}`);
-        console.log(user);
-    }     
-    else{
-        msg.send(`no se encontraron datos2 para ${name}`);
-    } */
   });
 };
