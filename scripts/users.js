efd8ba026377e4ec69ea9cebcae6e19005de1107
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-09-30 21:12:09 -0300

    test18

--- scripts/users.js
+++ scripts/users.js
@@ -33,12 +33,24 @@ module.exports = (robot) => {
   robot.respond(/user(.*)/i, (msg) => {
     const name = msg.match[1].split(' ')[1];
     var users = robot.brain.usersForFuzzyName(name);
+    var users2 = robot.brain.userForName(name);
+    var users3 = robot.brain.userForFuzzyName(name);
     //var users = getUserName(name);
     console.log(users);
     if(users.length > 0){
-        var user = users[0];
+        let user = users[0];
         //# Do something interesting here..
         res.send(`${name} is user - ${user}`);
+    }   
+    if(users2.length > 0){
+        let user = users2[0];
+        //# Do something interesting here..
+        res.send(`${name} is user2 - ${user}`);
+    }   
+    if(users3.length > 0){
+        let user = users3[0];
+        //# Do something interesting here..
+        res.send(`${name} is user3 - ${user}`);
     }   
   });
 };
