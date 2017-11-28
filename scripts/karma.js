b5ccea17320edee2b3f0575e4364f3486e0799e6
Christopher Vicuña <christopher.vicuna@gmail.com>
2017-11-28 14:46:15 -0300

    karma todos

--- scripts/karma.js
+++ scripts/karma.js
@@ -248,10 +248,10 @@ module.exports = robot => {
     res.setHeader('content-type', 'text/html')
     res.end(`<html>
     <head>
-      <title>devsChile - Karma Todos</title>
+      <title> - Karma Todos</title>
       <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Inconsolata:400,700"/>
       <link rel="stylesheet" type="text/css" href="//cdn.rawgit.com/mutable-tools/MutaGrid/master/demo/mutagrid/dist/5/mutagrid.min.css"/>
-      <style>body,html{height:100%;box-sizing:border-box}html{overflow-x:hidden}body{background:#000;color:#ddd;font-size:16px}body,code,pre{font-family:Inconsolata,monospace}code,h1,h2,h3,pre{color:#fff;font-weight:400}a{color:#e74c3c}.text-center{text-align:center}main{padding:5em 1.5em}h1{font-size:18px}h3{margin-top:20px}h2,h3{font-size:16px}hr{opacity:.4}</style>
+      <style>body,html{height:100%;box-sizing:border-box}html{overflow-x:hidden}body{background:#262626;color:#ddd;font-size:16px}body,code,pre{font-family:Inconsolata,monospace}code,h1,h2,h3,pre{color:#fff;font-weight:400}a{color:#e74c3c}.text-center{text-align:center}main{padding:5em 1.5em}h1{font-size:18px}h3{margin-top:20px}h2,h3{font-size:16px}hr{opacity:.4}</style>
     </head>
     <body>
       <main class="container">
